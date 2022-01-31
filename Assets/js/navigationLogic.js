228755865ab7a8a6bd069fc838c28e9ffa14f3f8
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-01-31 14:45:56 -0500

    Updating repo to match what i have on my local files

---
+++ Assets/js/navigationLogic.js
@@ -0,0 +1,11 @@
+let navSwapButton = document.getElementById(`navSwapIcon`);
+let classesWithClosed = document.querySelectorAll(`.nav-closed`);
+let classedWithOpen = document.querySelectorAll(`.nav-open`);
+
+for (let slider = 0; slider < array.length; index++) {
+    const element = array[index];
+    
+};
+
+
+navSwapButton.addEventListener(`click`, navigationSwap);
